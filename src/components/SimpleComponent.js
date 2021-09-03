ecb1d4d258608dbd1c540311652a6d5ef0b4690a
rmholsen <jaguar.kitty@gmail.com>
2021-09-03 11:49:02 -0400

    Completed

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,27 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react'
+
+class SimpleComponent extends Component {
+    state = {
+        mood: "happy"
+    }
+
+    render() {
+        return(
+            <div onClick={this.handleClick}>{this.state.mood}</div>
+        ) // sadly it's looking for a div, not a button
+        // yes, the HTML makes me twitch here
+    }
+
+    handleClick = () => {
+        // this.setState = "sad";
+        // no, this needs to toggle
+        const doom = this.state.mood === "happy" ? "sad" : "happy";
+        // LOOK I CAN DO TERNARY OPERATORS
+        this.setState({mood: doom});
+        // and mood has reached semantic saturation
+    }
+
+}
+
+export default SimpleComponent
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,16 @@
 // Code SimplerComponent Here
+
+import React from 'react'
+
+const SimplerComponent = props => <div onClick={props.handleClick}>"I am just happy</div>
+
+// YES, THIS ALSO WORKS
+// const SimplerComponent = (props) => {
+//     return(
+//         <div onClick={props.handleClick}>"I am just happy"</div>
+//     )
+// }
+
+export default SimplerComponent
+
+// for the love of Jacquard do not forget to import and export your crap. come on. 
